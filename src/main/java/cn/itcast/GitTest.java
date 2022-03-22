5ae85a564eaf856cb72627e0536002f06bdb6085
H.W <2396437178@qq.com>
2022-03-22 12:37:10 +0800

    third commit

--- src/main/java/cn/itcast/GitTest.java
+++ src/main/java/cn/itcast/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git222222");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git333333");
         System.out.println("Hello Git");
     }
 }
