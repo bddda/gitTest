782fd19ba151efff31721ccf2553cf4f84d248a7
H.W <2396437178@qq.com>
2022-03-22 12:51:08 +0800

    hot-fix commit1

--- src/main/java/cn/itcast/GitTest.java
+++ src/main/java/cn/itcast/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git222222");
         System.out.println("Hello Git333333");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git444444");
     }
 }
