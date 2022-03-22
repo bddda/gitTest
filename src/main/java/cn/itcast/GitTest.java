6da63da0a5c6c1ac7686df1c90d88943643b2521
H.W <2396437178@qq.com>
2022-03-22 12:35:51 +0800

    second commit

--- src/main/java/cn/itcast/GitTest.java
+++ src/main/java/cn/itcast/GitTest.java
@@ -3,7 +3,7 @@ package cn.itcast;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git222222");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
     }
